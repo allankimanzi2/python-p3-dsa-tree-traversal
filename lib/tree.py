2b491b5dd0aad88e7da4141cd916b1a11262edab
allankimanzi2 <allankim457@gmail.com>
2024-03-17 11:21:12 +0300

    Done assignment

--- lib/tree.py
+++ lib/tree.py
@@ -3,19 +3,19 @@ class Tree:
         self.root = root
 
     def get_element_by_id(self, id):
-        # Check if the root is None
+        
         if self.root is None:
             return None
         
-        # Depth-first traversal to find the node with the specified id
+        
         stack = [self.root]
         while stack:
             current_node = stack.pop()
             if current_node['id'] == id:
                 return current_node
-            # Add children to the stack for further traversal
+            
             for child in current_node.get('children', []):
                 stack.append(child)
         
-        # If the node with the specified id is not found, return None
+        
         return None
